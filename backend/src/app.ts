42b9531fa442bfc5e647f83a46d62013f3ecaa6e
Ashwin Parmar <103418850+parmarashwinvikrambhai@users.noreply.github.com>
2025-11-21 11:41:15 +0530

    Update app.ts
    change frontend url

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -18,7 +18,7 @@ app.use(cookieParser());
 
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://expencex-1.onrender.com"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
 }));
