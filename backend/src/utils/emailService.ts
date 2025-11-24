97e4c9a0b058028090076ace3679d5176f1df7b7
Ashwin <ap9320000@gmail.com>
2025-11-24 13:00:32 +0530

    id change again

--- backend/src/utils/emailService.ts
+++ backend/src/utils/emailService.ts
@@ -6,7 +6,7 @@ export const sendEmail = async (to: string, subject: string, html: string) => {
     try {
         await resend.emails.send({
             from: "Expense Tracker <onboarding@resend.dev>",
-            to: "ashwinparmar932000@gmail.com",  
+            to: "ap9320000@gmail.com",  
             subject,
             html,
         });
